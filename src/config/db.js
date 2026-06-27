39dbee5a0d4145c1e619d07046688b773df193d4
sakthi <sakthijessy26@gmail.com>
2026-06-27 12:02:31 +0530

    Support local and Neon database

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,23 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+const isProduction = process.env.DATABASE_URL;
+
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+      }
+);
 
 pool.connect((err, client, release) => {
   if (err) {
