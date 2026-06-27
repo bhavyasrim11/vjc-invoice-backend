bcd3cfe167f27e66c293cdb53fc5286e4802585e
sakthi <sakthijessy26@gmail.com>
2026-06-27 11:59:49 +0530

    Use DATABASE_URL for Neon

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,17 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.connect((err, client, release) => {
   if (err) {
-    console.error('Database connection error:', err.message);
+    console.error("Database connection error:", err.message);
   } else {
-    console.log('PostgreSQL Connected Successfully!');
+    console.log("PostgreSQL Connected Successfully!");
     release();
   }
 });
